97f8f382936ba0381902853ffd88d0acfc54bb96
Lim-jooyoung <mmporong@gmail.com>
2025-05-11 20:13:25 +0900

    FEAT: 에셋 이동 스크립트 병합

---
+++ Editor/Scripts/PackageAssetCopier.cs
@@ -0,0 +1,47 @@
+using UnityEditor;
+using UnityEngine;
+using System.IO;
+
+[InitializeOnLoad]
+public class PackageAssetCopier
+{
+    static PackageAssetCopier()
+    {
+        EditorApplication.delayCall += CopyFilesFromPackage;
+    }
+
+    public static void CopyFilesFromPackage()
+    {
+        // 프리팹 복사
+        CopyAssetFromPackage(
+            "Packages/com.dannect.toolkit/Runtime/Prefabs/Warning_Pop.prefab",
+            "Assets/04.Prefabs/Warning/Prefabs/Warning_Pop.prefab",
+            "프리팹"
+        );
+        
+        // 빌드 스크립트 복사
+        CopyAssetFromPackage(
+            "Packages/com.dannect.toolkit/Editor/Scripts/AutoWebGLBuild.cs",
+            "Assets/Editor/Scripts/AutoWebGLBuild.cs",
+            "빌드 스크립트"
+        );
+    }
+
+    private static void CopyAssetFromPackage(string packagePath, string projectPath, string assetType)
+    {
+        string absPackagePath = Path.GetFullPath(packagePath);
+        string absProjectPath = Path.GetFullPath(projectPath);
+
+        if (!File.Exists(absPackagePath))
+        {
+            Debug.LogWarning($"패키지 {assetType}을(를) 찾을 수 없습니다(아직 Import 중일 수 있음): {absPackagePath}");
+            return;
+        }
+
+        Directory.CreateDirectory(Path.GetDirectoryName(absProjectPath));
+        File.Copy(absPackagePath, absProjectPath, true);
+        AssetDatabase.Refresh();
+
+        Debug.Log($"패키지 {assetType}을(를) 프로젝트로 복사 완료!");
+    }
+} 
